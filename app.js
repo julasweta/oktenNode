930d20af259ae0da0951c75df212ca1e496116e5
julasweta <julasweta@ukr.net>
2023-09-18 23:46:09 +0300

    add: files included

--- app.js
+++ app.js
@@ -8,19 +8,25 @@ FOLDER: {folderName}
 /* FS */
 const fs = require('fs');
 const path = require('path');
-
-
-const folderPath = path.join(__dirname); // Замініть це на шлях до вашої папки
-
+const folderPath = path.join(__dirname);
 
 function checkFile(fileName) {
     if (fileName.includes('.')) {
-        console.log({FILE: fileName});
+        console.log('FILE:', fileName);
     } else {
-        console.log({FOLDER: fileName});
+        console.log('FOLDER:', fileName);
+        fs.readdir(path.join(__dirname, fileName), (err, files) => {
+            if (err) {
+                console.error('Помилка зчитування папки: ' + err);
+                return;
+            }
+            files.forEach((file) => {
+                console.log('File in Folder:', fileName);
+                checkFile(file);
+            });
+        });
     }
 }
-
 
 fs.readdir(folderPath, (err, files) => {
     if (err) {
@@ -28,8 +34,20 @@ fs.readdir(folderPath, (err, files) => {
         return;
     }
 
-
     files.forEach((file) => {
         checkFile(file);
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
